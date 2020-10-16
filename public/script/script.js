234f3e62457c47ddac70c920e92b5aec697b5bc6
= <=>
2020-10-15 23:49:02 -0400

    more script fixes

--- public/script/script.js
+++ public/script/script.js
@@ -1,32 +1,27 @@
-
 document.querySelector('.github').onclick = () => {
-	window.open('https://github.com/LinoMota','_blank')
+	window.open('https://github.com/LinoMota', '_blank')
 }
 
 document.querySelector('.linkedin').onclick = () => {
-	window.open('https://www.linkedin.com/in/lino-mota-494005197/','_blank')
+	window.open('https://www.linkedin.com/in/lino-mota-494005197/', '_blank')
 }
 
 document.querySelector('.instagram').onclick = () => {
-	window.open('https://www.instagram.com/lino.mota','_blank')
+	window.open('https://www.instagram.com/lino.mota', '_blank')
 }
 
 document.querySelector('.email').onclick = () => {
 	location.href = 'mailto:linomotadev@gmail.com'
 }
 
-
-
-function setVisibleClass(element,_class){
+function setVisibleClass(element, _class) {
 	element.style.opacity = (!element.classList.contains(_class)) ? "0" : "1"
 	element.style.display = (!element.classList.contains(_class)) ? "none" : ""
 }
 
-let tabs = document.querySelectorAll('.tabs a')
+document.querySelectorAll('.tabs a').forEach((tab, i) => {
 
-let itemClass = ['backend','web','tool']
-
-tabs.forEach( (tab,i) => {
+	let itemClass = ['backend', 'web', 'tool']
 
 	tab.onclick = () => {
 		let xpItems = document.querySelectorAll('.xp-item')
@@ -35,35 +30,41 @@ tabs.forEach( (tab,i) => {
 		})
 	}
 
-	tab.onkeydown = (e) =>{
-		if(e.keyCode == 13)
+	tab.onkeydown = (e) => {
+		if (e.keyCode == 13)
 			tab.click()
 	}
 })
 
-function loadJsonFile(filePath, callback){
+function loadJsonFile(filePath, callback) {
 
-	fetch(filePath).then( res => {
-		res.json().then( data => {
+	fetch(filePath).then(res => {
+		res.json().then(data => {
 			callback(data)
 		})
 	})
-	
+
 }
 
-function instertItems(jsonArray){
+function insertItems(jsonArray) {
 	let listDiv = document.querySelector('.item-list');
-	jsonArray.forEach( (item) => {
-		listDiv.innerHTML += '<div class="xp-item '+item.type+'">\
-								<div class="xp-image"><img src="./public/img/expIcons/'+item.svgImage+'"></div>\
-								<div class="xp-title">'+item.title+'</div>\
-								<div class="xp-desc">'+item.desc+'</div>\
-							</div>' ;
+	jsonArray.forEach(item => {
+		let {
+			type,
+			svgImage,
+			title,
+			desc
+		} = item
+		listDiv.innerHTML += `<div class="xp-item ${type}">\
+								<div class="xp-image"><img src="./public/img/expIcons/${svgImage}"></div>\
+								<div class="xp-title">${title}</div>\
+								<div class="xp-desc">${desc}</div>\
+							</div>`;
 	})
 }
 
-loadJsonFile('./public/data/backendExp.json',instertItems)
-loadJsonFile('../public/data/toolList.json',instertItems)
-loadJsonFile('../public/data/webExp.json',instertItems)
+loadJsonFile('../public/data/toolList.json', insertItems)
+loadJsonFile('../public/data/webExp.json', insertItems)
+loadJsonFile('./public/data/backendExp.json', insertItems)
 
-tabs[1].click()
+document.querySelectorAll('.tabs a')[0].click()
